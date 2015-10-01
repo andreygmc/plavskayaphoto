28d800f2e94b6a7d34f15edde32d21c545705898
Andrey Gorbatov <gorbatov@yandex-team.ru>
2015-10-01 23:33:43 +0300

    logo update.

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </a>
-                <a class="brand" href="/"><img src="assets/img/logo-new.png" width="120"/></a>
+                <a class="brand" href="/"><img src="assets/img/logo.png" width="120"/></a>
 
                 <div class="nav-collapse collapse">
                     <ul class="nav">
